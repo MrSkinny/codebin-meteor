c7ce3fd336fd04d2758821a531e1779da44f0127
Rich Greenhill <rgnet1@yahoo.com>
2016-10-02 10:28:59 -0700

    programmatic redirect when create bin

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import Accounts from './Accounts';
 
-import { Link } from 'react-router';
+import { Link, browserHistory } from 'react-router';
 
 class Header extends Component {
   onBinClick = (e) => {
     e.preventDefault();
 
-    Meteor.call('bins.insert');
+    Meteor.call('bins.insert', (err, binId) => {
+      if (err) return;
+      browserHistory.push(`/bins/${binId}`);
+    });
   }
 
   render() {

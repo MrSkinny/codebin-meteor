6f820608ba1798487eef04424b1838a0bdae8d91
Rich Greenhill <rgnet1@yahoo.com>
2016-10-02 09:48:33 -0700

    publish bins belonging to owner

--- server/main.js
+++ server/main.js
@@ -4,4 +4,7 @@ import _ from 'lodash';
 import { Bins } from '../imports/collections/bins';
 
 Meteor.startup(() => {
+  Meteor.publish('bins', function () {
+    return Bins.find({ ownerId: this.userId });
+  });
 });

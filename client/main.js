38fccb5776ac4735193dd01dee1c8b3f945d4b76
Rich Greenhill <rgnet1@yahoo.com>
2016-10-02 10:05:48 -0700

    root route

--- client/main.js
+++ client/main.js
@@ -1,10 +1,16 @@
-import { Meteor } from 'meteor/meteor';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
 import App from './components/App';
 import { Bins } from '../imports/collections/bins';
 
+const routes = (
+  <Router history={browserHistory}>
+    <Route path="/" component={App} />
+  </Router>
+);
+
 Meteor.startup(() => {
-  ReactDOM.render(<App />, document.getElementById('root'))
+  ReactDOM.render(routes, document.getElementById('root'))
 });

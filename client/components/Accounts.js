88c5c1114f3a6376b0a549b66cf62531255ee07d
Rich Greenhill <rgnet1@yahoo.com>
2016-10-01 17:41:56 -0700

    converted Blaze Accounts UI to react component

--- client/components/Accounts.js
+++ client/components/Accounts.js
@@ -1,9 +1,29 @@
+// General approach to use 3rd-party libs: Render in componentDidMount,
+// then clean up in componentWillUnmount
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { Template } from 'meteor/templating';
+import { Blaze } from 'meteor/blaze';
 
 class Accounts extends Component {
+  componentDidMount() {
+    // Render Blaze accounts form, then find div we just rendered and
+    // place Blaze form in the div
+    this._view = Blaze.render(
+      Template.loginButtons,
+      this._containerRef
+    );
+  }
+
+  componentWillUnmount() {
+    // Go find the Blaze forms we created and destroy them manually
+    Blaze.remove(this._view);
+  }
+
   render() {
     return (
-      <div></div>
+      <div ref={ref => this._containerRef = ref}>
+      </div>
     );
   }
 }
